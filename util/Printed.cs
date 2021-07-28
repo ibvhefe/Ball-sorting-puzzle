57f583134a4c358a636f619fccf11024c7612d2e
ibvhefe <sebastian.mechelke@hotmail.de>
2021-07-28 20:56:17 +0200

    update Printed.cs

--- util/Printed.cs
+++ util/Printed.cs
@@ -1,1 +1,22 @@
-
+class ConsolePrinter
+{
+  void Print(List<SolvingStep> solution)
+  {
+    for(var j=0; j<=solution.First().GetLength(1)-1; j++)
+    {
+    for(var s=0; s<=solution.length-1; s++)
+    {
+      for(var i=0; i<=solution.First().Board.GetLength(0)-1; i++)
+      {
+        var content = solution[s].Board[i,j];
+        if(content>9)
+        {
+          Console.Write(" ");
+        }
+        Console.Write($"  {content}");
+      }
+    }
+    Console.WriteLine();
+    }
+  }
+}
