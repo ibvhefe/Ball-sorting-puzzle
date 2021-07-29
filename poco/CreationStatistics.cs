133b63e8fc2073900cd961ca7244b34dec5a9bb4
ibvhefe <sebastian.mechelke@hotmail.de>
2021-07-29 21:40:06 +0200

    update CreationStatistics.cs

--- poco/CreationStatistics.cs
+++ poco/CreationStatistics.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 public class CreationStatistics
 {
+    public byte[,] Riddle { get; set; }
     public List<List<SolvingStep>> Solutions { get; set; }
     public int NodeCount { get; set; }
     public int SolutionNodeCount { get; set; }
