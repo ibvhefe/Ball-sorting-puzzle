2150494622f0da8838faf477b1da09f5b410bc3f
ibvhefe <sebastian.mechelke@hotmail.de>
2021-07-31 01:06:39 +0200

    added unit test (really painful experience)

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ball_sorting_puzzle;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NoDuplicateSolutions()
+        {
+            byte cupSize = 4;
+            byte cupCount = 3;
+            byte colorCount = 2;
+            var riddleCreator = new RiddleCreator(cupSize, cupCount, colorCount);
+            var riddleSolver = new RiddleSolver(cupSize, cupCount, colorCount);
+
+            // Workflow steps.
+            var randomRiddle = riddleCreator.Create();
+            var gameTree = riddleSolver.Solve(randomRiddle);  
+
+            var count = gameTree.Solutions.Count;
+		    var distinctCount = gameTree.Solutions.Distinct(new SolutionComparer()).Count();
+            Assert.IsTrue(count == distinctCount);
+        }
+    }
+}
