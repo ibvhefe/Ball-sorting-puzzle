e7b0e3cc7177e5dd977086b64bba53e722f9eafc
ibvhefe <sebastian.mechelke@hotmail.de>
2021-08-01 23:10:51 +0200

    Added unit test

--- UnitTests/SampleRiddleTests.cs
+++ UnitTests/SampleRiddleTests.cs
@@ -32,5 +32,27 @@ namespace UnitTests
             var gameTree = riddleSolver.Solve(unsolveableRiddle);
             Assert.AreEqual(0, gameTree.Solutions.Count);
         }
+
+        [TestMethod]
+        public void WrongSolutionPossible()
+        {
+            var unsolveableRiddle = new byte[3,4];
+            unsolveableRiddle[0,0]=1;
+            unsolveableRiddle[0,1]=1;
+            unsolveableRiddle[0,2]=0;
+            unsolveableRiddle[0,3]=0;
+            unsolveableRiddle[1,0]=2;
+            unsolveableRiddle[1,1]=2;
+            unsolveableRiddle[1,2]=1;
+            unsolveableRiddle[1,3]=0;
+            unsolveableRiddle[2,0]=1;
+            unsolveableRiddle[2,1]=2;
+            unsolveableRiddle[2,2]=2;
+            unsolveableRiddle[2,3]=0;
+
+            var riddleSolver = new RiddleSolver(4, 3, 2);
+            var gameTree = riddleSolver.Solve(unsolveableRiddle);
+            Assert.AreEqual(2, gameTree.Solutions.Count);
+        }
     }
 }
