69deed58ef2ca9e17330f34cd24f5e847f2a2f6c
ibvhefe <sebastian.mechelke@hotmail.de>
2021-08-11 23:26:55 +0200

    Corrected WrongSolutionPossible Test. Enabled ConsolePrinter to print in debug mode.

--- UnitTests/SampleRiddleTests.cs
+++ UnitTests/SampleRiddleTests.cs
@@ -36,22 +36,29 @@ namespace UnitTests
         [TestMethod]
         public void WrongSolutionPossible()
         {
-            var unsolveableRiddle = new byte[3,4];
+            var unsolveableRiddle = new byte[4,4];
             unsolveableRiddle[0,0]=1;
-            unsolveableRiddle[0,1]=1;
-            unsolveableRiddle[0,2]=0;
+            unsolveableRiddle[0,1]=3;
+            unsolveableRiddle[0,2]=1;
             unsolveableRiddle[0,3]=0;
             unsolveableRiddle[1,0]=2;
             unsolveableRiddle[1,1]=2;
             unsolveableRiddle[1,2]=1;
             unsolveableRiddle[1,3]=0;
-            unsolveableRiddle[2,0]=1;
-            unsolveableRiddle[2,1]=2;
-            unsolveableRiddle[2,2]=2;
-            unsolveableRiddle[2,3]=0;
+            unsolveableRiddle[2,0]=3;
+            unsolveableRiddle[2,1]=3;
+            unsolveableRiddle[2,2]=3;
+            unsolveableRiddle[2,3]=1;
+            unsolveableRiddle[3,0]=2;
+            unsolveableRiddle[3,1]=2;
+            unsolveableRiddle[3,2]=0;
+            unsolveableRiddle[3,3]=0;
 
-            var riddleSolver = new RiddleSolver(4, 3, 2);
+            var riddleSolver = new RiddleSolver(4, 4, 3);
             var gameTree = riddleSolver.Solve(unsolveableRiddle);
+            ConsolePrinter.SetDebugMode();
+            ConsolePrinter.Print(gameTree);
+
             Assert.AreEqual(2, gameTree.Solutions.Count);
         }
     }
