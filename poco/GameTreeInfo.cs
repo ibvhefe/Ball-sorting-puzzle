7515a0e5a4f57408229800bd3999e5499d738faf
ibvhefe <sebastian.mechelke@hotmail.de>
2021-11-13 12:54:07 +0100

    Added comments

--- poco/GameTreeInfo.cs
+++ poco/GameTreeInfo.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 public class GameTreeInfo
 {
     public byte[,] Riddle { get; set; }
+
+    /// All possible solutions.
     public List<List<SolvingStep>> Solutions { get; set; }
+
+    /// Once inside a deadend group, there is no way to win again.
     public List<HashSet<SolvingStep>> DeadendNodeGroups { get; set; }
+    
+    /// Overall node count.
     public int NodeCount { get; set; }
+
+    /// All nodes that do not belong to deadend groups and not directly to solutions.
     public int CanReachSolutionCount { get; set; }
+
+    /// Total count of all nodes and leafs belonging to solutions.
     public int SolutionNodeCount { get; set; }
+
+    /// Total count of all deadend nodes and leafs.
     public int DeadendNodeCount { get; set; }
 }
